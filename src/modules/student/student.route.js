f3820dd6ebb927aa78abe440fe3326c2a8e29970
Danny Ho <hocong.danh16@gmail.com>
2026-05-13 11:44:11 +0700

    feat: add student routes with getAllStudents endpoint

--- src/modules/student/student.route.js
+++ src/modules/student/student.route.js
@@ -3,7 +3,6 @@ import * as studentController from './student.controller.js';
 
 const router = express.Router();
 
-// Endpoint: GET /api/v1/student/all
 router.get('/', studentController.getAllStudents);
 
 export default router;
