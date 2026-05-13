e8dbf2e74df856f51ed5ca32899c786064ec9278
Danny Ho <hocong.danh16@gmail.com>
2026-05-13 16:46:40 +0700

    stable project state

--- src/modules/student/student.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import * as studentController from './student.controller.js';
-
-const router = express.Router();
-
-router.get('/', studentController.getAllStudents);
-
-export default router;
