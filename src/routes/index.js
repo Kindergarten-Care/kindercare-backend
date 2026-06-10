e71ed15e8763e40c8addeccbbdae57c47240a52a
Nguyen Minh Chanh <nguyenminhchanh27@gmail.com>
2026-06-10 14:21:28 +0700

    feat: implement endpoint to get users by role and group them

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import authRoute from '../modules/auth/auth.route.js';
+import userRoute from '../modules/user/user.route.js';
 
 const router = express.Router();
 
 const defaultRoutes = [
     { path: '/auth', route: authRoute },
+    { path: '/users', route: userRoute },
 ];
 
 defaultRoutes.forEach((route) => {
